1b4d32874460673daf7d297ad9f0bcf6aa6fa808
Ralph Cacho <cacho.rc@gmail.com>
2022-06-20 18:14:23 -0700

    ex: use inline snapshot for password error

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -46,5 +46,7 @@ test('omitting the password results in an error', async () => {
 
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"password required"`,
+  )
 })
